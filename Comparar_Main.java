ebacb3e37574fd85a052f0b5106efc614525b028
madeleinnehenao <124703743+madeleinnehenao@users.noreply.github.com>
2023-04-06 03:25:42 -0500

    Add files via upload

---
+++ Comparar_Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compararnumeros;
+
+/**
+ *
+ * @author PAULINA HENAO GARCIA
+ */
+public class Comparar_Main {
+
+    public static void main(String[] args) {
+        Comparar_Numeros Ejercicio7=new Comparar_Numeros();
+        
+        Ejercicio7.setVisible(true);
+        Ejercicio7.INICIAR_PROGRAMA();
+    }
+    
+}
