d22fe52ebe0c871deaa41cf75fc86299460b7df5
madeleinnehenao <124703743+madeleinnehenao@users.noreply.github.com>
2023-04-09 14:11:14 -0500

    Update Calculadora_Raiz_Cubo_Cuadrado.java

--- Calculadora_Raiz_Cubo_Cuadrado.java
+++ Calculadora_Raiz_Cubo_Cuadrado.java
@@ -16,7 +16,7 @@ public class Calculadora_Raiz_Cubo_Cuadrado extends JFrame {
     private JButton BCalcular,BBorrar,BCerrar;
     
     public Calculadora_Raiz_Cubo_Cuadrado(){
-        setBounds(50,50,600,500);//Tamaño de mi Ventana.
+        setBounds(50,50,800,500);//Tamaño de mi Ventana.
         setLocationRelativeTo(null);
         setTitle("CALCULADORA");//Nombre de mi ventana.
         setDefaultCloseOperation(EXIT_ON_CLOSE);//Cierro mi ventana.
@@ -36,61 +36,61 @@ public class Calculadora_Raiz_Cubo_Cuadrado extends JFrame {
     
     }
     public void ETIQUETAS_CAJAS(){    
-        Numero=new JLabel("INGRESE UN NUMERO");
-        Numero.setBounds(30,10,200,30);
+        Numero=new JLabel("INGRESE LOS NUMEROS SEPARADOS POR COMA");
+        Numero.setBounds(30,10,380,30);
         panel.add(Numero);
         
         Num=new JTextField();
-        Num.setBounds(240,10,250,30);
+        Num.setBounds(400,10,250,30);
         panel.add(Num);
         Num.requestFocusInWindow();
     }
 
     public void BOTONES(){
         BCalcular=new JButton("CALCULAR");
-        BCalcular.setBounds(30,300,110,30);
+        BCalcular.setBounds(100,300,110,30);
         panel.add(BCalcular);
         BCalcular.requestFocusInWindow();        
         
         BBorrar=new JButton("BORRAR");
-        BBorrar.setBounds(220,300,110,30);
+        BBorrar.setBounds(340,300,110,30);
         panel.add(BBorrar);
         BBorrar.requestFocusInWindow();
         
         BCerrar=new JButton("CERRAR");
-        BCerrar.setBounds(400, 300, 110, 30);
+        BCerrar.setBounds(580, 300, 110, 30);
         panel.add(BCerrar);
         BCerrar.requestFocusInWindow();
         
         Raiz=new JLabel();
-        Raiz.setBounds(30,50,200,30);
+        Raiz.setBounds(30,50,400,30);
         Raiz.setVisible(false);
         panel.add(Raiz);
         
         Cuadrado=new JLabel();
-        Cuadrado.setBounds(30,100,200,30);
+        Cuadrado.setBounds(30,100,400,30);
         Cuadrado.setVisible(false);
         panel.add(Cuadrado);
         
         Cubo=new JLabel();
-        Cubo.setBounds(30,150,200,30);
+        Cubo.setBounds(30,150,400,30);
         Cubo.setVisible(false);
         panel.add(Cubo);
         
         raiz=new JTextField();
-        raiz.setBounds(240,50,250,30);
+        raiz.setBounds(400,50,250,30);
         panel.add(raiz);
         raiz.setVisible(false);
         raiz.requestFocusInWindow();
 
         cuadrado=new JTextField();
-        cuadrado.setBounds(240,100,250,30);
+        cuadrado.setBounds(400,100,250,30);
         panel.add(cuadrado);
         cuadrado.setVisible(false);
         cuadrado.requestFocusInWindow();
 
         cubo=new JTextField();
-        cubo.setBounds(240,150,250,30);
+        cubo.setBounds(400,150,250,30);
         panel.add(cubo);
         cubo.setVisible(false);
         cubo.requestFocusInWindow();
@@ -98,21 +98,32 @@ public class Calculadora_Raiz_Cubo_Cuadrado extends JFrame {
         ActionListener oyenteDeCalcular=new ActionListener(){   
         @Override
         public void actionPerformed(ActionEvent e) {
-            int num=Integer.parseInt(Num.getText());
-            double rz=Math.sqrt(num);
-            double cua=num*num;
-            double cbo=num*num*num;
-            Raiz.setText("LA RAIZ DE "+num+" ES:");
+            String number=Num.getText();
+            String [] valores=number.split(",");
+            StringBuilder raices=new StringBuilder();
+            StringBuilder cuadrados =new StringBuilder();
+            StringBuilder cubos =new StringBuilder();
+
+            for(String valor:valores){
+                double num=Double.parseDouble(valor.trim());
+                double rz=Math.round(Math.sqrt(num));
+                raices.append(rz).append(", ");
+                double cua=num*num;
+                cuadrados.append(cua).append(", ");
+                double cbo=num*num*num;
+                cubos.append(cbo).append(", ");
+            }
+            Raiz.setText("LAS RAICES DE LOS NUMEROS SON:");
             Raiz.setVisible(true);
-            Cuadrado.setText("EL CUADRADO DE "+num+" ES:");
+            Cuadrado.setText("EL CUADRADO DE LOS NUMEROS SON:");
             Cuadrado.setVisible(true);
-            Cubo.setText("EL CUBO DE "+num+" ES:");
+            Cubo.setText("EL CUBO DE DE LOS NUMEROS SON:");
             Cubo.setVisible(true);
-            raiz.setText(String.valueOf(rz));
+            raiz.setText(String.valueOf(raices));
             raiz.setVisible(true);
-            cuadrado.setText(String.valueOf(cua));
+            cuadrado.setText(String.valueOf(cuadrados));
             cuadrado.setVisible(true);
-            cubo.setText(String.valueOf(cbo));
+            cubo.setText(String.valueOf(cubos));
             cubo.setVisible(true);
 
         }
